90cd60a56104c53bf963dabed7cfb7f08bf76be0
CorruptBIOS <82524442+CorruptBIOS@users.noreply.github.com>
2021-12-16 08:07:28 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ module.exports = ctx => ({
   description: 'Welcome to the (unofficial) home of Pinewood!',
   dest: 'public/',
   themeConfig: {
-    repo: 'https://gitlab.com/pinewood-builders/pinewood-website',
+    repo: 'https://gitlab.com/pinewood-builders/handbooks/pinewood/pinewood-website',
     editLinks: true,
     docsDir: 'docs/',
     logo: '/PB-Logo.png',
